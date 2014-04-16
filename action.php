ceb722826dacc4ecc60824049f17f661417f2369
Alexander Tsvetkov <ff.warprobot@gmail.com>
2014-04-16 18:34:20 +0400

    refactoring

--- action.php
+++ action.php
@@ -3,18 +3,12 @@
     header ("Location: http://iu3-50.ru");
 
     function file_download($filename) {
-    // Проверяем существование файла
       if (file_exists($filename)) {
-    // Здесь пишем код, который будет обрабатывать каждую загрузку файла.
-
-    //  Перенаправляем клиента на файл.
         header('Location: ' . $filename);
       } else {
-    // Если файл не найден, сообщаем клиенту об этом через заголовки HTTP
         header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
         header('Status: 404 Not Found');
       }
-    // Прерываем дальнейшее выполнение скрипта, чтобы не отправлять мусор в ответе клиенту
       exit;
     }
 ?>
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
-
+    // Smoothy scrolling to anchor.
     $('a[href*=#]').on('click', function(event){
-    event.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top-20}, 500);
-});
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top-20}, 500);
+    });
 });
